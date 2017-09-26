36d29c16aa0198e97e59bccd1723472961e0b107
System <none>
2017-09-26 20:53:10 +0000

    Checkpoint

---
+++ src/app.vue
@@ -0,0 +1,28 @@
+<style>
+  .red {
+    color: #f00;
+  }
+</style>
+
+<template>
+  <div>
+    <h1 class="red">{{msg}}</h1>
+    <button @click="test">lol {{num}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello world!',
+      num: 0
+    }
+  },
+  methods: {
+    test() {
+      this.num++
+    }
+  }
+}
+</script>
